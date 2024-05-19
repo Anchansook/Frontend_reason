d3144ecb2690f47820660e8d4b24ee998e062cd2
Anchansook <154656462+Anchansook@users.noreply.github.com>
2024-05-19 17:51:37 +0900

    0519 수업 내용

---
+++ E-react/src/views/0519/c_Axios/Axios04.tsx
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React from 'react'
+
+//! HTTP 통신 - PUT(수정) / DELETE(삭제)
+
+export default function Axios04() {
+  const userId = 1; // 예제용 ID (정적 데이터)
+
+  const updateUser = async () => {  
+    try {
+      // put 메서드
+      // : 2개의 인자를 전달
+      // >> 첫 번째 인자인 URL에서 수정하고자 하는 데이터를 명시
+      // >> 두 번째 인자에서 수정 데이터를 명시
+      const response = await axios.put(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+        name: '안찬숙',
+        email: '안찬숙이메일'
+      });
+      console.log('User updated', response.data);
+    } catch(error :any) {
+      console.error('Failed to Update user: ' + error.message);
+    }
+  };
+
+  const deleteUser = async () => {
+    try {
+      // delete 메서드
+      // : 지정된 URL의 데이터만 삭제
+      // >> 데이터 전송 없이, URL만을 사용하여 삭제 가능
+      const response = await axios.delete(`https://jsonplaceholder.typicode.com/users/${userId}`);
+
+      // 삭제 후 데이터 출력 >> 빈 값
+      console.log('user deleted', response.data);
+    } catch(error :any) {
+      console.log('Failed to delete user', error.message);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={updateUser}>Update User</button>
+      <button onClick={deleteUser}>Delete User</button>
+    </div>
+  )
+}
--- E-react/src/views/0519/index.tsx
+++ E-react/src/views/0519/index.tsx
@@ -1,4 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import BlogPosts from "./a_Router/BlogPosts";
+import BlogPost from "./a_Router/BlogPost";
+import QueryParams01 from "./b_QueryParams/QueryParams01";
+import QueryParams02 from "./b_QueryParams/QueryParams02";
+import Axios01 from "./c_Axios/Axios01";
+import Axios02 from "./c_Axios/Axios02";
+import Axios03 from "./c_Axios/Axios03";
+import Axios04 from "./c_Axios/Axios04";
+import ContextApi01 from "./d_ContextApi/ContextApi01";
+import ThemeProvider from "./d_ContextApi/ThemeProvider";
 
 export default function Index() {
 
@@ -13,8 +24,33 @@ export default function Index() {
       </h1>
 
       <h2>1. Practice</h2>
+      <Routes>
+        {/* 중첩된 라우터에서 빈 /는 메인 경로를 의미 */}
+        <Route path="/" element={<BlogPosts />} />
+        <Route path="/posts/:postId" element={<BlogPost />} />
+      </Routes>
 
-      <h2>2. Global State</h2>
+      <h2>2. QueryParams & useSearchParams</h2>
+      <Routes>
+        <Route path="/" element={<QueryParams01 />} />
+        <Route path="/product" element={<QueryParams02 />} />
+      </Routes>
+
+      <h2>3. Axios</h2>
+      <Axios01 />
+      <Axios02 />
+      <Axios03 />
+      <Axios04 />
+
+      <h2>4. Context API - 전역 상태 관리</h2>
+      <ContextApi01 />
+      
+      <ThemeProvider>
+        <div>
+          <h1>안녕하세요 테마 스위치 앱입니다.</h1>
+          <p>해당 내용들은 ThemeProvider의 컴포넌트들입니다.</p>
+        </div>
+      </ThemeProvider>
 
     </div>
   );
